2b2c53f434c5e0ee8e0ce71796977cfb4322c381
Navaneeth <knavaneethkumar8@gmail.com>
2026-01-19 20:36:22 +0530

    added new origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,7 @@
 const allowedOrigins = [
   'http://127.0.0.1:5500',
-  'http://127.0.0.1:5505'
+  'http://127.0.0.1:5505',
+  'https://akshar-neon.vercel.app'
 ]
-
-
 
 module.exports = allowedOrigins;
